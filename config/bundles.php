27130746448dc1d10ad91f46b067cc02fd1f0e1d
Phalcorine <phalcorine@gmail.com>
2021-10-16 18:58:11 +0100

    Added Nelmio CORS bundle for CORS...

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     SymfonyBundles\JsonRequestBundle\JsonRequestBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
